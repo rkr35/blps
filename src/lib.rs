a0b4cf330e1c8cfae0e5415bf6f830e561b949f8
rkr35 <rkr35@dragons.drexel.edu>
2020-05-30 18:51:39 -0400

    Refactor game structures to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
 #![warn(clippy::pedantic)]
 
-use std::borrow::Cow;
-use std::ffi::CStr;
 use std::fs::File;
 use std::io::{self, BufWriter, Read, Write};
-use std::iter;
-use std::ops::Deref;
-use std::os::raw::c_char;
 use std::ptr;
-use std::slice;
 
 use log::{error, info};
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
@@ -24,112 +18,20 @@ use winapi::{
     },
 };
 
+
+mod game;
+use game::{Objects, Names};
+
 mod macros;
+
 mod module;
 use module::Module;
-
-pub type Objects = Array<*mut Object>;
-pub type Names = Array<*const Name>;
 
 const OBJECTS: &str = "objects.txt";
 const NAMES: &str = "names.txt";
 
 pub static mut GLOBAL_OBJECTS: *const Objects = ptr::null();
 pub static mut GLOBAL_NAMES: *const Names = ptr::null();
-
-#[repr(C)]
-pub struct Array<T> {
-    data: *mut T,
-    count: u32,
-    max: u32,
-}
-
-impl<T> Deref for Array<T> {
-    type Target = [T];
-
-    fn deref(&self) -> &Self::Target {
-        unsafe {
-            slice::from_raw_parts(self.data, self.count as usize)
-        }
-    }
-}
-
-#[repr(C)]
-pub struct Name {
-    pad0: [u8; 0x10],
-    text: c_char,
-}
-
-impl Name {
-    pub unsafe fn text(&self) -> Cow<str> {
-        CStr::from_ptr(&self.text as *const c_char).to_string_lossy()
-    }
-}
-
-#[repr(C)]
-pub struct NameIndex {
-    index: u32,
-    number: u32,
-}
-
-#[repr(C)]
-pub struct Object {
-    vtable: usize,
-    pad0: [u8; 0x1c],
-    pub index: u32,
-    pad1: [u8; 0x4],
-    pub outer: *mut Object,
-    name: NameIndex,
-    class: *mut Struct,
-    pad2: [u8; 0x4],
-}
-
-impl Object {
-    pub unsafe fn full_name(&self) -> Option<String> {
-        if self.class.is_null() {
-            return None;
-        }
-
-        let outer_names: Option<Vec<_>> = self.iter_outer().map(|o| o.name()).collect();
-        let mut outer_names = outer_names?;
-        outer_names.reverse();
-        let name = outer_names.join(".");
-
-        let class = String::from((*self.class).field.object.name()?);
-
-        Some(class + " " + &name)
-    }
-
-    pub unsafe fn iter_outer(&self) -> impl Iterator<Item = &Self> {
-        iter::successors(Some(self), |current| current.outer.as_ref())
-    }
-
-    pub unsafe fn name(&self) -> Option<Cow<str>> {
-        let name = *(*GLOBAL_NAMES).get(self.name.index as usize)?;
-
-        if name.is_null() {
-            None
-        } else {
-            Some((*name).text())
-        }
-    }
-}
-
-#[repr(C)]
-pub struct Field {
-    object: Object,
-    pub next: *mut Field,
-}
-
-#[repr(C)]
-pub struct Struct {
-    field: Field,
-    pad0: [u8; 8],
-    pub super_field: *mut Struct,
-    pub children: *mut Field,
-    pub property_size: u16,
-    pad1: [u8; 0x3a],
-}
 
 fn idle() {
     println!("Idling. Press enter to continue.");
