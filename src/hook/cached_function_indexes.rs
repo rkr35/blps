c766cdf5f1613aad68bd4a422e5edf260b6e6e7a
rkr35 <rkr35@dragons.drexel.edu>
2020-07-05 18:28:26 -0400

    Add PostRender, player tick, and player destroyed

--- src/hook/cached_function_indexes.rs
+++ src/hook/cached_function_indexes.rs
@@ -10,12 +10,16 @@ pub enum Error {
 
 pub struct CachedFunctionIndexes {
     pub post_render: u32,
+    pub player_tick: u32,
+    pub player_destroyed: u32,
 }
 
 impl CachedFunctionIndexes {
     pub unsafe fn new() -> Result<Self, Error> {
         Ok(Self {
             post_render: find("Function WillowGame.WillowGameViewportClient.PostRender")?,
+            player_tick: find("Function WillowGame.WillowPlayerController.PlayerTick")?,
+            player_destroyed: find("Function WillowGame.WillowPlayerController.Destroyed")?,
         })
     }
 }
--- src/hook/user/mod.rs
+++ src/hook/user/mod.rs
@@ -1,19 +1,55 @@
-use crate::game::{Function, Object};
+use crate::game::{cast, Function, Object};
+use crate::hook::sdk::{Canvas, WillowPlayerController};
+
 use super::CACHED_FUNCTION_INDEXES;
 
 use std::ffi::c_void;
+use std::ptr;
 
 use log::info;
 
 mod yank;
 use yank::Yank;
 
-pub fn process_event(_this: &Object, method: &Function, _parameters: *mut c_void, _return_value: *mut c_void) {
-    let indexes = unsafe { CACHED_FUNCTION_INDEXES.yank_ref() };
-    
-    if method.index == indexes.post_render {
-        if let Some(full_name) = unsafe { method.full_name() } {
-            info!("{}", full_name);
+pub static mut CONTROLLER: *mut WillowPlayerController = ptr::null_mut();
+
+pub unsafe fn process_event(this: *mut Object, method: *mut Function, parameters: *mut c_void, _return_value: *mut c_void) {
+    let indexes = CACHED_FUNCTION_INDEXES.yank_ref();
+    let method_index = (*method).index;
+
+    if method_index == indexes.post_render {
+        my_post_render(parameters.cast());
+    } else if method_index == indexes.player_tick {
+        my_player_tick(this.cast());
+    } else if method_index == indexes.player_destroyed {
+        my_player_destroyed();
+    } else {
+        // print_event(this, method);
+    }
+}
+
+unsafe fn my_post_render(canvas: *mut *mut Canvas) {
+    let canvas = *canvas;
+    (*canvas).SetPos(100.0, 100.0, 0.0);
+    (*canvas).DrawBox(200.0, 200.0);
+}
+
+unsafe fn my_player_tick(my_controller: *mut WillowPlayerController) {
+    if CONTROLLER.is_null() {
+        CONTROLLER = my_controller;
+        info!("Set CONTROLLER.");
+    }
+}
+
+unsafe fn my_player_destroyed() {
+    CONTROLLER = ptr::null_mut();
+    info!("Destroyed CONTROLLER.");
+}
+
+fn print_event(object: &Object, method: &Function) {
+    if let Some(object) = unsafe { object.full_name() } {
+        if let Some(method) = unsafe { method.full_name() } {
+            info!("{} called {}", object, method);
         }
     }
 }
